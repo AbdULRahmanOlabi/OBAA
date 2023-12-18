d48198e9c73315a3870ce36beb45dd104e998596
AbdULRahmanOlabi <abd.alrahman.olabi@hotmail.com>
2023-12-18 17:25:45 +0300

    V15.0

--- contact.php
+++ contact.php
@@ -1,30 +1,29 @@
-<?php 
-  $name = $_POST['name'];
-  $email = $_POST['email'];
-  $subject = $_POST['subject'];
-  $message = $_POST['message'];
-  
-  // Set the email header for HTML content
-  $emailheader =  "Content-Type: text/html; charset=UTF-8\r\n";
-  $emailheader .= "From: $name <$email>\r\n"; // English terms are commonly used in email headers
+<?php
+$name = $_POST['name'];
+$email = $_POST['email'];
+$subject = $_POST['subject'];
+$message = $_POST['message'];
 
-  $recipient = "abd.alrahman.olabi@gmail.com"; // Company Email (That Will Receive The Mail)
+// Set the email header for HTML content
+$emailheader =  "Content-Type: text/html; charset=UTF-8\r\n";
+$emailheader .= "From: $name <$email>\r\n"; // English terms are commonly used in email headers
 
-  // Create the body of the email with right-to-left direction
-  $body = "<div><strong>Name:</strong> $name<br><strong>Email:</strong> $email<br><strong>Subject:</strong> $subject<br><br>$message</div>";
+$recipient = "abd.alrahman.olabi@gmail.com"; // Company Email (That Will Receive The Mail)
 
-  $email_subject = "Omar Bin Abd Al-Azeez Private School";
+// Create the body of the email with right-to-left direction
+$body = "<div><strong>Name:</strong> $name<br><strong>Email:</strong> $email<br><strong>Subject:</strong> $subject<br><br>$message</div>";
 
-  // Sending the email
-  if (mail($recipient, $email_subject, $body, $emailheader)) {
-    echo '<script>
+$email_subject = "Omar Bin Abd Al-Azeez Private School";
+
+// Sending the email
+if (mail($recipient, $email_subject, $body, $emailheader)) {
+  echo '<script>
     alert("Email Sent Successfully")
     window.location.replace("index-En");
     </script>';
-  } else {
-    echo '<script>
+} else {
+  echo '<script>
     alert("Sorry, Failed While Sending Mail!")
     window.location.replace("index-En");
     </script>';
-  }
-?>
+}
